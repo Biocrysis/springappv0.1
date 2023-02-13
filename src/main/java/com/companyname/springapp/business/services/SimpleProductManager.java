270c6cf646b4eb20ff339dd2b645d98323eb8dbd
Biocrysis <uriel_2012_666@hotmail.com>
2023-02-13 15:14:14 -0800

    se termino de crear el test para los metodos de obtener productos y detalles

--- src/main/java/com/companyname/springapp/business/services/SimpleProductManager.java
+++ src/main/java/com/companyname/springapp/business/services/SimpleProductManager.java
@@ -2,31 +2,39 @@ package com.companyname.springapp.business.services;
 
 /**
  * Clase que implementa los metodos de la interfaz
+ * 
  * @author uriel
  * @since 13/02/2023
  * @version 1.0
+ * 
  */
+
 import java.util.List;
 
 import com.companyname.springapp.business.entities.Product;
 
-public class SimpleProductManager implements ProductManager{
-	@Override
+public class SimpleProductManager implements ProductManager {
+	
+private List<Product> products;
+
+
 	public List<Product> getProducts() {
-		// TODO Auto-generated method stub
-		//excepcion para marcar que el o los metodos no estan implementados.
-		throw new UnsupportedOperationException();
+		
+		// excepcion para marcar que el o los metodos no estan implementados.
+		//throw new UnsupportedOperationException();
+		
+		return products;
 	}
 
-	@Override
 	public void increasePrice(int percentage) {
-		// TODO Auto-generated method stub
+		
 		throw new UnsupportedOperationException();
+	}
+	
+	
+	public void setProducts(List<Product> products) {
+		//throw new UnsupportedOperationException();
+		this.products=products;
 	}
 
-	public void serProducts(List<Product> products) {
-		throw new UnsupportedOperationException();
-	}
-	
-	
 }
