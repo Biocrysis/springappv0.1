daac0bd17a304848e3480f013014147f6320cba5
Biocrysis <uriel_2012_666@hotmail.com>
2023-02-14 15:47:09 -0800

    Añadir un controlador de formulario

--- src/main/java/com/companyname/springapp/web/controllers/PriceIncreaseFormController.java
+++ src/main/java/com/companyname/springapp/web/controllers/PriceIncreaseFormController.java
@@ -1,11 +1,23 @@
 package com.companyname.springapp.web.controllers;
 
+import java.lang.ProcessBuilder.Redirect;
+
+import javax.validation.Valid;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.companyname.springapp.business.services.PriceIncrease;
+import com.companyname.springapp.business.services.ProductManager;
 
 /**
  * Clase para mapear url para aunmentar el precio de un producto
- * atraves del un formulario
+ * atraves del  formulario
  * @author uriel
  * @since 14/02/2023
  * @version 1.0
@@ -15,5 +27,43 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 @RequestMapping(value = "/priceincrease.htm")
 public class PriceIncreaseFormController {
+	
+    /** Logger for this class and subclasses */
+    protected final Log logger = LogFactory.getLog(getClass());
+    //*
+    
+    @Autowired
+    private ProductManager productManager;
+    
+    /**
+     * 
+     * @param priceIncrease
+     * @param result
+     * @return
+     */
+    
+    @RequestMapping(method = RequestMethod.POST)
+    public String onSubmit(@Valid PriceIncrease priceIncrease , BindingResult result) {
+    	if(result.hasErrors()) {
+    		return "priceincrease";
+    	}
+    	
+    	int increase= priceIncrease.getPercentage();
+    	logger.info("Increasing prices by " + increase + "%.");
+    	
+    	productManager.increasePrice(increase);
+    	
+    	return "redirect:/hell.htm";
+    	}
+    /**
+     * 
+     * @return return priceIncrease regrese el 15 para el valor de incremento
+     */
+    @RequestMapping(method = RequestMethod.GET)
+    protected PriceIncrease formBackingObjet() {
+    	PriceIncrease priceIncrease = new PriceIncrease();
+    	priceIncrease.setPercentage(15);
+    	return priceIncrease;
+    }
 
 }
