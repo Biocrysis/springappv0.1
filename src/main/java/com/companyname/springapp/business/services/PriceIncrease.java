a15b85e8e9da0104ff20c2e6b58903cecf7a9a94
Biocrysis <uriel_2012_666@hotmail.com>
2023-02-14 15:07:39 -0800

    se creo JavaBean  muy sencillo que solamente contiene una propiedad, con sus correspondientes métodos getter y setter

---
+++ src/main/java/com/companyname/springapp/business/services/PriceIncrease.java
@@ -0,0 +1,34 @@
+package com.companyname.springapp.business.services;
+
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+/** Clase JavaBean.
+ *  La cual sers el objeto que el formulario rellene
+ * y desde el que, nuestra lógica de negocio extraerá el porcentaje de incremento que queremos aplicar a los precios
+ * @author uriel
+ *
+ */
+
+public class PriceIncrease {
+	
+	 /** Logger for this class and subclasses */
+    protected final Log logger = LogFactory.getLog(getClass());
+    
+    @Min(0)
+    @Max(50)
+    private int percentage;
+
+	public int getPercentage() {
+		return percentage;
+	}
+
+	public void setPercentage(int i) {
+		percentage=i;
+		logger.info("percentage set to: "+ i );
+	}
+
+}
---
+++ src/main/java/com/companyname/springapp/web/controllers/PriceIncreaseFormController.java
@@ -0,0 +1,19 @@
+package com.companyname.springapp.web.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * Clase para mapear url para aunmentar el precio de un producto
+ * atraves del un formulario
+ * @author uriel
+ * @since 14/02/2023
+ * @version 1.0
+ *
+ */
+
+@Controller
+@RequestMapping(value = "/priceincrease.htm")
+public class PriceIncreaseFormController {
+
+}
